d0f80057af8a477cd6feef86dc3848679940c986
jcallin <j.callin@hotmail.com>
2015-11-10 14:48:49 -0800

    Articles are now held in objects, need to be interp and stored in a db

--- pyscraper.py
+++ pyscraper.py
@@ -1,13 +1,19 @@
 from util import * 
 
 #RSS Urls
-rss_urls = ["http://feeds.bbci.co.uk/news/rss.xml?edition=int#", "http://www.nytimes.com/services/xml/rss/nyt/InternationalHome.xml"]
+rss_urls = ["http://feeds.bbci.co.uk/news/world/rss.xml#", "http://www.nytimes.com/services/xml/rss/nyt/InternationalHome.xml"]
 
 feeds = getFeed(rss_urls)
 
 feedObjs =  [parseFeed(feed) for feed in feeds]
 
+#Display root feed info, place news into separate objects
 for feedObj in feedObjs:
     print feedObj.title
     print feedObj.url
     print feedObj.description
+    articles = [MyArticle(item["link"], item["title"], item["published_parsed"]) for item in feedObj.items]
+
+#Display each article title, these will be interpreted
+for article in articles:
+    print article.title
--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import urllib
 import feedparser
-#Contains functions for parsing and organizing feed data
+import re
+
+#Functions for parsing and organizing feed data
 
 #Takes a list of URLs and returns a list of raw feed objects [?index?: feed]
 def getFeed(urls):
@@ -9,20 +11,18 @@ def getFeed(urls):
 
     return feeds
 
-
 def parseFeed(feed):
     #Takes a feed and returns a list of custom MyFeed objects
 
-    feedObj = MyFeed(feed["url"], feed["channel"]["title"], feed["channel"][ "description"])
+    feedObj = MyFeed(feed["url"], feed["channel"]["title"], feed["channel"][ "description"], feed["items"])
 
     return feedObj
-
 
 class MyFeed:
     'Base class for our feeds'
     feedCount=0
 
-    def __init__(self, url, title, description):
+    def __init__(self, url, title, description, items):
         self.url=url
         self.title=title
 
@@ -31,4 +31,16 @@ class MyFeed:
         else:
             self.description="NO DESCRIPTION AVAILABLE"
 
+        self.items=[item for item in items]
+
         MyFeed.feedCount+=1
+
+class MyArticle:
+    'Base class for each article'
+    articleCount=0
+
+    def __init__(self, url, title, date_parsed):
+        self.url=url
+        self.title=title
+        self.date_parsed=date_parsed
+        MyArticle.articleCount+=1
