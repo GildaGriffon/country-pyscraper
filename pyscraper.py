3a30bcf15515bc98edf0fc1a11aa77c0ad176b0f
jcallin <j.callin@hotmail.com>
2015-11-10 03:05:40 -0800

    Added function to retrieve feed info, class to hold feed info, NYT feed

--- pyscraper.py
+++ pyscraper.py
@@ -1,10 +1,13 @@
 from util import * 
 
 #RSS Urls
-rss_urls = ["http://feeds.bbci.co.uk/news/rss.xml?edition=int#"]
+rss_urls = ["http://feeds.bbci.co.uk/news/rss.xml?edition=int#", "http://www.nytimes.com/services/xml/rss/nyt/InternationalHome.xml"]
 
 feeds = getFeed(rss_urls)
 
-for feed in feeds:
-    print feed
+feedObjs =  [parseFeed(feed) for feed in feeds]
 
+for feedObj in feedObjs:
+    print feedObj.title
+    print feedObj.url
+    print feedObj.description
--- util.py
+++ util.py
@@ -1,13 +1,34 @@
 import urllib
 import feedparser
+#Contains functions for parsing and organizing feed data
 
-#Takes a list of URLs and returns a list of feeds [index: feed]
+#Takes a list of URLs and returns a list of raw feed objects [?index?: feed]
 def getFeed(urls):
 
-    #Aggregate feeds into a list
     feeds = [feedparser.parse(url) for url in urls] 
 
-    for feed in feeds:
-        print feed
-       
     return feeds
+
+
+def parseFeed(feed):
+    #Takes a feed and returns a list of custom MyFeed objects
+
+    feedObj = MyFeed(feed["url"], feed["channel"]["title"], feed["channel"][ "description"])
+
+    return feedObj
+
+
+class MyFeed:
+    'Base class for our feeds'
+    feedCount=0
+
+    def __init__(self, url, title, description):
+        self.url=url
+        self.title=title
+
+        if(description):
+            self.description=description
+        else:
+            self.description="NO DESCRIPTION AVAILABLE"
+
+        MyFeed.feedCount+=1
